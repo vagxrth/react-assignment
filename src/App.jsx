d8ee2285903dd50b6ee5734a2eb4bf5a6b4c3beb
Vagarth <vagarth419@gmail.com>
2024-07-13 23:21:22 +0530

    feat: create different routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-/*
- * Temporary problems array schema
- */
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Problems from "./pages/Problems";
+import ProblemDetails from "./pages/ProblemDetails";
+
+
+
 const problems = [{
     title: "201. Bitwise AND of Numbers Range",
     difficulty: "Medium",
     acceptance: "42%"
-},{
+}, {
     title: "201. Bitwise AND of Numbers Range",
     difficulty: "Medium",
     acceptance: "412%"
 },
-    {
-        title: "202. Happy Number",
-        difficulty: "Easy",
-        acceptance: "54.9%"
-    },
-    {
-        title: "203. Remove Linked List Elements",
-        difficulty: "Hard",
-        acceptance: "42%"
-    }];
+{
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "54.9%"
+},
+{
+    title: "203. Remove Linked List Elements",
+    difficulty: "Hard",
+    acceptance: "42%"
+}];
 
 
 function App() {
@@ -31,11 +37,19 @@ function App() {
        /problems/:problem_slug - A single problem page
      */
 
+
+
     return (
-    <div>
-        Finish the assignment! Look at the comments in App.jsx as a starting point
-    </div>
-  )
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/problems" element={<Problems />} />
+                <Route path="/problems/:title" element={<ProblemDetails />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 // A demo component
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className='text-red-500 text-4xl font-bold'>
+            Leetcode
+        </h1>
+
+        <div>
+            <button>
+                Login
+            </button>
+            <button>
+                Signup
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
